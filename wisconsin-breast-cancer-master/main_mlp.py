7fd3eb33f628f73bc2279f91e157805f129e241d
juanhaobobi <148977577+juanhaobobi@users.noreply.github.com>
2023-10-26 18:07:00 +0800

    Update main_mlp.py

--- wisconsin-breast-cancer-master/main_mlp.py
+++ wisconsin-breast-cancer-master/main_mlp.py
@@ -1,52 +1,23 @@
-# Copyright 2017 Abien Fred Agarap
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =========================================================================
-
 """Main program implementing the MLP class"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-__version__ = "0.1"
-__author__ = "Abien Fred Agarap"
-
 import argparse
 from models.MLP import MLP
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 BATCH_SIZE = 128
 LEARNING_RATE = 1e-2
 NUM_CLASSES = 2
 NUM_NODES = [500, 500, 500]
 
-
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="MLP written using TensorFlow, for Wisconsin Breast Cancer Diagnostic Dataset"
+        description="MLP written using TensorFlow 2.x, for Wisconsin Breast Cancer Diagnostic Dataset"
     )
     group = parser.add_argument_group("Arguments")
     group.add_argument(
         "-n", "--num_epochs", required=True, type=int, help="number of epochs"
-    )
-    group.add_argument(
-        "-l",
-        "--log_path",
-        required=True,
-        type=str,
-        help="path where to save the TensorBoard logs",
     )
     group.add_argument(
         "-r",
@@ -57,7 +28,6 @@ def parse_args():
     )
     arguments = parser.parse_args()
     return arguments
-
 
 def main(arguments):
     # load the features of the dataset
@@ -76,6 +46,9 @@ def main(arguments):
         features, labels, test_size=0.30, stratify=labels
     )
 
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_features, train_labels)).batch(BATCH_SIZE)
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_features, test_labels)).batch(BATCH_SIZE)
+
     model = MLP(
         alpha=LEARNING_RATE,
         batch_size=BATCH_SIZE,
@@ -86,16 +59,10 @@ def main(arguments):
 
     model.train(
         num_epochs=arguments.num_epochs,
-        log_path=arguments.log_path,
-        train_data=[train_features, train_labels],
-        train_size=train_features.shape[0],
-        test_data=[test_features, test_labels],
-        test_size=test_features.shape[0],
-        result_path=arguments.result_path,
+        train_dataset=train_dataset,
+        test_dataset=test_dataset,
     )
 
-
-if __name__ == "__main__":
-    args = parse_args()
-
-    main(args)
+    # Example of saving predictions
+    # predictions = model.predict(test_dataset)  # Assuming you have a predict method in MLP class
+    # model.save_labels(predictions=predictions, actual=test_labels, result_path=arguments.result_path,
